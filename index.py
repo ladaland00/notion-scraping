c59b64cc95d2b5046939ec514293761b5f787742
ladaland <ladamoneone@gmail.com>
2023-10-30 23:27:53 +0700

    all data

--- index.py
+++ index.py
@@ -70,18 +70,141 @@ try:
         try:  
             eventName = tableData.find_element( By.XPATH,"./div/div/div[2]/div/div/a/span")
             print(eventName.text)
-            organizerType = tableData.find_element( By.XPATH,"./div/div/div[3]")
-            print(organizerType.text)
-            attendees = tableData.find_element( By.XPATH,"./div/div/div[4]")
-            print(attendees.text)
-            opportunities = tableData.find_element( By.XPATH,"./div/div/div[5]")
-            print(opportunities.text)
+            if eventName ==None:
+                continue 
+            try:
+                organizerType = tableData.find_element( By.XPATH,"./div/div/div[3]")
+            except NoSuchElementException:
+                opportunities = None           
+            try:
+                attendees = tableData.find_element( By.XPATH,"./div/div/div[4]")
+            except NoSuchElementException:
+                attendees = None                   
+            try:
+                opportunities = tableData.find_element( By.XPATH,"./div/div/div[5]")
+            except NoSuchElementException:
+                opportunities = None 
+            try:
+                attendance = tableData.find_element( By.XPATH,"./div/div/div[6]")
+            except NoSuchElementException:
+                attendance = None               
+            try:
+                startDate = tableData.find_element( By.XPATH,"./div/div/div[7]")
+            except NoSuchElementException:
+                startDate = None        
+            try:
+                endDate = tableData.find_element( By.XPATH,"./div/div/div[8]")
+            except NoSuchElementException:
+                endDate = None                       
+            try:
+                website = tableData.find_element( By.XPATH,"./div/div/div[9]")
+            except NoSuchElementException:
+                website = None   
+            try:
+                website = tableData.find_element( By.XPATH,"./div/div/div[10]")
+            except NoSuchElementException:
+                website = None  
+            try:
+                sponsorshipSite = tableData.find_element( By.XPATH,"./div/div/div[11]")
+            except NoSuchElementException:
+                sponsorshipSite = None 
+            try:
+                sponsorDownload = tableData.find_element( By.XPATH,"./div/div/div[12]")
+            except NoSuchElementException:
+                sponsorDownload = None 
+            try:
+                linkedIn = tableData.find_element( By.XPATH,"./div/div/div[12]")
+            except NoSuchElementException:
+                linkedIn = None 
+            try:
+                attendeeList = tableData.find_element( By.XPATH,"./div/div/div[13]")
+            except NoSuchElementException:
+                attendeeList = None 
+            try:
+                venue = tableData.find_element( By.XPATH,"./div/div/div[14]")
+            except NoSuchElementException:
+                venue = None 
+            try:
+                city = tableData.find_element( By.XPATH,"./div/div/div[15]")
+            except NoSuchElementException:
+                city = None 
+            try:
+                stateOrProvince = tableData.find_element( By.XPATH,"./div/div/div[16]")
+            except NoSuchElementException:
+                stateOrProvince = None 
+            try:
+                country = tableData.find_element( By.XPATH,"./div/div/div[17]")
+            except NoSuchElementException:
+                country = None 
+            try:
+                lowPriceNonMembers = tableData.find_element( By.XPATH,"./div/div/div[18]")
+            except NoSuchElementException:
+                lowPriceNonMembers = None 
+            try:
+                highPriceNonMembers = tableData.find_element( By.XPATH,"./div/div/div[19]")
+            except NoSuchElementException:
+                highPriceNonMembers = None 
+            try:
+                earlyBirdDeadline = tableData.find_element( By.XPATH,"./div/div/div[20]")
+            except NoSuchElementException:
+                earlyBirdDeadline = None 
+            try:
+                sponsorshipContactEmail = tableData.find_element( By.XPATH,"./div/div/div[21]")
+            except NoSuchElementException:
+                sponsorshipContactEmail = None 
+            try:
+                sponsorshipContactName = tableData.find_element( By.XPATH,"./div/div/div[22]")
+            except NoSuchElementException:
+                sponsorshipContactName = None 
+            try:
+                filesAndMedia = tableData.find_element( By.XPATH,"./div/div/div[23]")
+            except NoSuchElementException:
+                filesAndMedia = None 
+            try:
+                latestSponsorList = tableData.find_element( By.XPATH,"./div/div/div[24]")
+            except NoSuchElementException:
+                latestSponsorList = None 
+            try:
+                sponsorsAndExhibitors = tableData.find_element( By.XPATH,"./div/div/div[25]")
+            except NoSuchElementException:
+                sponsorsAndExhibitors = None 
+            try:
+                peekURL = tableData.find_element( By.XPATH,"./div/div/div[26]")
+            except NoSuchElementException:
+                peekURL = None 
+            try:
+                notes = tableData.find_element( By.XPATH,"./div/div/div[27]")
+            except NoSuchElementException:
+                notes = None 
             if eventName :
                 cardData.append({
                     "eventName": eventName.text, 
                     "organizerType": organizerType.text.replace("\n", ", "),
                     "attendees": attendees.text.replace("\n", ", "),
-                    "opportunities": opportunities.text.replace("\n", ", ")
+                    "opportunities": opportunities.text.replace("\n", ", "),
+                    "attendance":attendance.text,
+                    "start": startDate.text,
+                    "end": endDate.text,
+                    "website":website.text,
+                    "sponsorshipSite":sponsorshipSite.text,
+                    "sponsorDownload":sponsorDownload.text,
+                    "linkedIn":linkedIn.text,
+                    "attendeeList":attendeeList.text,
+                    "venue": venue.text,
+                    "city":city.text,
+                    "stateOrProvince":stateOrProvince.text,
+                    "country":country.text,
+                    "lowPriceNonMembers":lowPriceNonMembers.text,
+                    "highPriceNonMembers":highPriceNonMembers.text,
+                    "earlyBirdDeadline":earlyBirdDeadline.text,
+                    "sponsorshipContactEmail":sponsorshipContactEmail.text,
+                    "sponsorshipContactName":sponsorshipContactName.text,
+                    "filesAndMedia":filesAndMedia.text,
+                    "latestSponsorList":latestSponsorList.text,
+                    "sponsorsAndExhibitors":sponsorsAndExhibitors.text,
+                    "peekURL":peekURL.text,
+                    "notes":notes.text
+                    
                 })
             
         except NoSuchElementException:
